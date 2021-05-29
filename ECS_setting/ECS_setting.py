aecc94d98266e0d4b94a43f15034fbb085b0b43c
Copynacht <zxxp1341@gmail.com>
2021-05-29 16:36:23 +0900

    first push

--- ECS_setting/ECS_setting.py
+++ ECS_setting/ECS_setting.py
@@ -19,50 +19,52 @@ def encode(key, clear):
 
 
 s = requests.Session()
+try:
+    print("ユーザー登録")
+    user = input("user:")
+    password = input("password:")
+    # ログインに使うURL
+    url = "https://cas.ecs.kyoto-u.ac.jp/cas/login?service=https://panda.ecs.kyoto-u.ac.jp/sakai-login-tool/container"
 
-print("ユーザー登録")
-user = input("user:")
-password = input("password:")
-# ログインに使うURL
-url = "https://cas.ecs.kyoto-u.ac.jp/cas/login?service=https://panda.ecs.kyoto-u.ac.jp/sakai-login-tool/container"
+    # ログインフォームからLTを抜き出す
+    r1 = s.get(url)
+    soup = BeautifulSoup(r1.content, 'html.parser')
+    for input in soup.select('form input'):
+        if input['name'] == 'lt':
+            lt = input['value']
 
-# ログインフォームからLTを抜き出す
-r1 = s.get(url)
-soup = BeautifulSoup(r1.content, 'html.parser')
-for input in soup.select('form input'):
-    if input['name'] == 'lt':
-        lt = input['value']
+    # ログイン処理
+    params = {'_eventId': 'submit', 'execution': 'e1s1', 'lt': lt,
+                'username': user, 'password': password}
+    params = urllib.parse.urlencode(params)
+    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+    res = s.post(url=url, data=params, headers=headers)
 
-# ログイン処理
-params = {'_eventId': 'submit', 'execution': 'e1s1', 'lt': lt,
-            'username': user, 'password': password}
-params = urllib.parse.urlencode(params)
-headers = {'Content-Type': 'application/x-www-form-urlencoded'}
-res = s.post(url=url, data=params, headers=headers)
+    if(res.url != 'https://panda.ecs.kyoto-u.ac.jp/portal'):
+        raise Exception
 
-if(res.url != 'https://panda.ecs.kyoto-u.ac.jp/portal'):
-    raise Exception
+    euser = encode("pand", user)
+    epass = encode("pand", password)
 
-euser = encode("pand", user)
-epass = encode("pand", password)
+    conf = configparser.ConfigParser()
+    if os.path.exists(INI):
+        conf.read(INI)
+    if not "user" in conf.sections():
+        conf.add_section("user")
+    conf.set("user", "user", euser)
+    conf.set("user", "password", epass)
+    # ファイルに書き込む
 
-conf = configparser.ConfigParser()
-if os.path.exists(INI):
-    conf.read(INI)
-if not "user" in conf.sections():
-    conf.add_section("user")
-conf.set("user", "user", euser)
-conf.set("user", "password", epass)
-# ファイルに書き込む
-
-
-if not os.path.exists("C:/ProgramData/SinPandA"):
-    os.mkdir("C:/ProgramData/SinPandA")
-    os.mkdir("C:/ProgramData/SinPandA/data")
-f = open(INI, "w")
-conf.write(f)
-f.close()
-print("ユーザー登録成功")
-
-
-sys.exit()
+    try:
+        if not os.path.exists("C:/ProgramData/SinPandA"):
+            os.mkdir("C:/ProgramData/SinPandA")
+            os.mkdir("C:/ProgramData/SinPandA/data")
+        f = open(INI, "w")
+        conf.write(f)
+        f.close()
+        print("ユーザー登録成功")
+    except:
+        print("ユーザー設定書き込み失敗")
+except:
+    print("ユーザー登録失敗")
+    
